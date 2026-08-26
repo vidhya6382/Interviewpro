20f32b55ebd66cea90bf9650eca0d3ea9f1a9f05
vidhya6382 <vidhv501@gmail.com>
2026-08-26 14:16:53 +0530

    fix 404 routing

---
+++ api/index.py
@@ -0,0 +1,5 @@
+﻿import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'myproject', 'backend'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+from backend.asgi import application as app
