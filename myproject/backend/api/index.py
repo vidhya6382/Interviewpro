563f06aaf77171870c67651df70482b68c1835c1
vidhya6382 <vidhv501@gmail.com>
2026-08-26 13:54:26 +0530

    switch to api/index.py wrapper

---
+++ myproject/backend/api/index.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+from backend.wsgi import application
+
+app = application
