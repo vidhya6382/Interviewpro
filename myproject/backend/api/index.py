77305e15b9fbb2f64d4b71395638a75e90725b51
vidhya6382 <vidhv501@gmail.com>
2026-08-26 14:13:07 +0530

    fix vercel entry

--- myproject/backend/api/index.py
+++ myproject/backend/api/index.py
@@ -1,10 +1,1 @@
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-from backend.wsgi import application
-
-app = application
+from myproject.backend.backend.asgi import application as app
