f287aad93794823212876757759421a9b3bdca28
vidhya6382 <vidhv501@gmail.com>
2026-08-26 14:19:19 +0530

    fix encoding

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
-﻿import os
-import sys
+import os, sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'myproject', 'backend'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 from backend.asgi import application as app
